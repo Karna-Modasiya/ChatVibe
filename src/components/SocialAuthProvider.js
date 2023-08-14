b6e630ff8b086ba6c44cf3d0804071ff78f07ccd
Karna <karanmodasiya24@gmail.com>
2023-08-14 23:20:45 +0530

    Added Facebook Login

--- src/components/SocialAuthProvider.js
+++ src/components/SocialAuthProvider.js
@@ -4,13 +4,17 @@ import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import Snackbar from 'react-native-snackbar'
 import Colors from '../constants/Colors';
 import { GoogleSignin,statusCodes } from '@react-native-google-signin/google-signin';
+import { LoginManager, AccessToken,Settings } from 'react-native-fbsdk-next';
 import auth from '@react-native-firebase/auth';
+import { useAuth } from '../utils/AuthContext';
 
 const SocialLogin = ({
     iconName,
     title,
     ...props
 }) => {
+
+    const {setAuthType} = useAuth();
 
     useEffect(() => {
         GoogleSignin.configure({ webClientId: '871654022468-i99ke0tlge5cs7440kn58to6ltos6na4.apps.googleusercontent.com' });
@@ -31,7 +35,11 @@ const SocialLogin = ({
             console.log("Google Credentials : ",googleCredential);
 
             // Sign-in the user with the credential
-            auth().signInWithCredential(googleCredential);
+            auth().signInWithCredential(googleCredential)
+                .then(()=>{
+                    setAuthType("google");
+                    console.log("Logged in using google")
+                });
         } catch (error) {
             console.log(error);
             if (error.code === statusCodes.SIGN_IN_CANCELLED) {
@@ -54,10 +62,55 @@ const SocialLogin = ({
             }
         }
     }
+
+    const handleFacebookAuth = async() => {
+        // Attempt login with permissions
+        const result = await LoginManager.logInWithPermissions(['public_profile', 'email']);
+
+        if (result.isCancelled) {
+            Snackbar.show({
+                text: 'Cancelled sign in ',
+                duration: Snackbar.LENGTH_SHORT,
+              })
+        }
+
+        // Once signed in, get the users AccessToken
+        const data = await AccessToken.getCurrentAccessToken();
+
+        if (!data) {
+            Snackbar.show({
+                text: 'Something went wrong',
+                duration: Snackbar.LENGTH_SHORT,
+              })
+        }
+        // console.log(data);
+        // Create a Firebase credential with the AccessToken
+        const facebookCredential = auth.FacebookAuthProvider.credential(data.accessToken);
+
+        // Sign-in the user with the credential
+        auth().signInWithCredential(facebookCredential)
+            .then(() => {
+                setAuthType("facebook");
+                console.log('LOgged in using facebook');
+            })
+            .catch(error => {
+                setLoading(false);
+                if (error.code === 'auth/account-exists-with-different-credential') {
+                    console.log('Account Already Exists');
+                    Snackbar.show({
+                    text: 'Account Already Exists',
+                    duration: Snackbar.LENGTH_SHORT,
+                    })
+                }
+                console.error(error);
+            });
+    }
     
     const handleAuthProviders = ()=>{
         if(title === "Google")
             handleGoogleAuth();
+        else if(title === "Facebook")
+            handleFacebookAuth();
     }
 
     return (
